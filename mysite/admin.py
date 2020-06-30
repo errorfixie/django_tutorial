61f3dc0a1f046ed25d975d11246ce2a588801e3f
errorfixie <65066542+errorfixie@users.noreply.github.com>
2020-06-30 16:00:00 +0900

    개요

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Reporter, Article
+
+admin.site.register(Article)
+admin.site.register(Reporter)
--- mysite/hello.py
+++
@@ -1,3 +0,0 @@
-from .models import Reporter, Article
-
-Reporter.objects.all()
