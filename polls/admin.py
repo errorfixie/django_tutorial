4e4e12258d4f64cb77c73cce05409e08c3a36f1e
errorfixie <65066542+errorfixie@users.noreply.github.com>
2020-07-01 11:56:15 +0900

    장고 tutorial part 2
    장고 tutorial part 2
    
    요약)
    장고는 모델로 데이터베이스를 관리한다.
    admin을 통해 웹에서 데이터 변경,추가 가능하다.
    
    사용모듈)
    django.contrib.admin
    django.db.models
    
    궁금해서 찾아본 것)
    ** 프레임워크 = 어플리케이션을 만드는 공장
    ** API = 어려운것과 내 프로그램을 이어주는 연결도구
    ** migrations = 장고에서 데이터 모델을 저장한 파일
    ** migrate = migration들을 이용해 데이터베이스 생성, 변경으로 동기화
    ** 스키마 = (데이터베이스를 구성하는 레코드의 크기, 키(key)의 정의, 레코드와 레코드의 관계, 검색 방법 등을 정의한 것.)

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# from .models import Reporter, Article
+
+# admin.site.register(Article)
+# admin.site.register(Reporter)
+
+from .models import Question, Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
