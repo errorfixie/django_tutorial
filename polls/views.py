c0a600c61e656b7fc97869bc6ed63a3da5e1c985
errorfixie <65066542+errorfixie@users.noreply.github.com>
2020-07-01 15:50:50 +0900

    vote form 까지

--- polls/views.py
+++ polls/views.py
@@ -26,6 +26,26 @@ def results(request,question_id):
     response = "너는 질문의 결과를 보고 있다. %s."
     return HttpResponse(response % question_id)
 
-def vote(request, question_id):
-    return HttpResponse("질문에 투표하고 있다 %s." % question_id)
+# def vote(request, question_id):
+#     return HttpResponse("질문에 투표하고 있다 %s." % question_id)
 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+def vote(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{'question':question,
+        'error_message': "선택을 안했어",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
