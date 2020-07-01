d293e8483df66707ef98656369ec94b85c4c9e48
errorfixie <65066542+errorfixie@users.noreply.github.com>
2020-07-01 14:42:24 +0900

    장고 tutorial part 3
    장고 tutorial part 3
    
    요약)
    장고는 들어온 url에서 urlconfs을 이용하여 view에 접근한다
    view는 Httpresponse객체를 반환하거나 예외를 내거나 둘 중 하나를 한다.
    view는 파이썬의 모든 라이브러리를 사용할 수 있다.
    템플릿폴더 밑에 앱 이름의 폴더을 또 만드는 이유는 장고가 템플릿파일을 찾을때 앱을 구별하지 못하기 때문이다.
    앱이름의 폴더를 만들어 장고가 템플릿파일을 찾을때 앱을 구별하도록 해준다.
    
    1. 뷰에서 직접 웹페이지 구현 2. 템플릿으로 보내 웹페이지 구현
    뷰에서 처리한 것을 template으로 보내 템플릿에서 처리하고 Httpresponse로 리턴

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django.http import Http404
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {'latest_question_list':latest_question_list,}
+#     return HttpResponse(template.render(context,request))
 
 def index(request):
-    return HttpResponse("Hello. world")
-# Create your views here.
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list,}
+    return render(request,'polls/index.html',context)
+
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html',{'question':question})
+    # return HttpResponse("질문을 보고 있다 %s." % question_id)
+
+def results(request,question_id):
+    response = "너는 질문의 결과를 보고 있다. %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("질문에 투표하고 있다 %s." % question_id)
+
